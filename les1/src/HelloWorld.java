b62f2b40b2868224de74b67d709b206266444473
Dima <graiwers@gmail.com>
2026-07-10 18:02:37 +0400

    'les1'

---
+++ les1/src/HelloWorld.java
@@ -0,0 +1,24 @@
+public class HelloWorld {
+
+    public int add(int num1, int num2){
+        int result = num1 + num2;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    // public - открытый модификатор доступа.
+    // Контроль доступа классу или методу другими частями программ
+
+    // void - метод ничего не возвращает
+    // Тип возвращаемого значения
+
+    // параметр (String[] args) - массив строк
+    // массив аргументов, каждый аргумент является строкой,
+    // передаваемый в java из командной строки
+
+    //при выполнении класса, выполняет метод с именем main
+
+}
